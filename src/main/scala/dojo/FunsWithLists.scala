f49cc231a8aff17ed4e51c53732aff7bf71b9e5b
Todd Owen <todd.m.owen@gmail.com>
2012-04-09 17:18:22 +0800

    Solution to first four problems.

--- src/main/scala/dojo/FunsWithLists.scala
+++ src/main/scala/dojo/FunsWithLists.scala
@@ -4,13 +4,32 @@ import collection.immutable.List
 
 object FunsWithLists {
 
-  def labels(ls: List[Game]) = Nil
+  def labels(ls: List[Game]) = ls map (_.label)
 
-  def averageRatingsOf(l:String,  ls:List[Game]) = 0
+  def averageRatingsOf(l:String,  ls:List[Game]) = {
+    /* 1 BONUS POINT : complete this using only higher order functions */
+    val criteria: (Game => Boolean) = (_.label == l)
+    val gameCount = ls count criteria
+    val ratingsTotal = (ls filter criteria).foldLeft(0) (_ + _.rating)
+    List(ratingsTotal,gameCount) reduce (_/_)
+  }
 
-  def totalRatingsOf(ls: List[Game]):Int = 0
+  def totalRatingsOf(ls: List[Game]):Int = ls match {
+    /* 1 BONUS POINT : complete this using recursion */
+    case Nil     => 0
+    case g :: gs => g.rating + totalRatingsOf(gs)
+  }
 
-  def totalRatingsOfLabel(label: String, list: List[Game]):Int = 0
+  def totalRatingsOfLabel(label: String, list: List[Game]):Int = {
+    /* 1 BONUS POINT : complete this in an imperative style */
+    var total = 0
+    list foreach { game =>
+      if (game.label == label) {
+        total += game.rating
+      }
+    }
+    return total
+  }
 
   def increaseRatingBy(inc: Int, ls: List[Game]) = Nil
 
